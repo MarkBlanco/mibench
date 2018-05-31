0fadbfa09fd48c837a549229859b81331b3d3f3a
MarkBlanco <markb1@andrew.cmu.edu>
2018-05-31 14:59:30 +0000

    Update make and clean scripts.

--- clean-all.sh
+++ clean-all.sh
@@ -1,27 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 # A script to make all the MiBench tests for an embedded target.
-srcdirs="automotive/basicmath
-         automotive/bitcount
-         automotive/qsort
-         automotive/susan
-         consumer/jpeg/jpeg-6a
-         consumer/tiff-v3.5.4
-         consumer/typeset/lout-3.24"
-         #consumer/mad/mad-0.14.2b
-srcdirs+=" consumer/lame/lame3.70
-         office/ispell"
-         #office/rsynth
-         #office/sphinx
-srcdirs+=" office/stringsearch
-         security/blowfish"
-         #security/pgp
-srcdirs+=" security/rijndael
-         security/sha"
-         #telecomm/adpcm/src
-srcdirs+=" telecomm/CRC32
-         telecomm/FFT"
-         #telecomm/gsm
+declare -a srcdirs=(
+	"automotive/basicmath"
+    "automotive/bitcount"
+    "automotive/qsort"
+    "automotive/susan"
+    "consumer/jpeg/jpeg-6a"
+    "consumer/tiff-v3.5.4"
+    "consumer/typeset/lout-3.24"
+    #"consumer/mad/mad-0.14.2b"
+	"consumer/lame/lame3.70"
+    "office/ispell"
+    #"office/rsynth"
+    #"office/sphinx"
+	"office/stringsearch"
+    "security/blowfish"
+    #"security/pgp"
+	"security/rijndael"
+    "security/sha"
+    #"telecomm/adpcm/src"
+	"telecomm/CRC32"
+    "telecomm/FFT"
+    #"telecomm/gsm"
+	)
 
 
 export CC=gcc #arc-linux-uclibc-gcc
@@ -29,7 +31,7 @@ export RUNIT=run-remote.sh
 
 BASE_DIR=$(pwd)
 
-for d in ${srcdirs}
+for d in ${srcdirs[@]}
 do
     echo ${d}
     cd ${d}
