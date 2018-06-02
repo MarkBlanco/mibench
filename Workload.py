693775fc7b8c0d7e7c242c9c560f30bc4eea7b19
Mark Blanco <mark.p.blanco@gmail.com>
2018-06-02 13:32:37 -0400

    Add working code for serially executing collections of MiBench benchmarks using python and specified in JSON.

---
+++ Workload.py
@@ -0,0 +1,83 @@
+from multiprocessing import Process, Pipe
+import os, sys
+import subprocess
+import time, random
+
+def rand_affinity():
+    affinity_l = 0#random.randint(0, 15)
+    affinity_b = random.randint(1, 1) #15)
+    affinity = affinity_l | (affinity_b << 4)
+    affinity_string = hex(affinity)
+    num_threads = bin(affinity).count("1")#random.randint(1, bin(affinity).count("1"))
+    return affinity_string, num_threads
+
+
+# This function is meant to run as a separate process that runs each microbenchmark
+# of a workload in sequence:
+def run_workload(affinity_string, num_threads, executables, name, stats_pipe):
+    cmd = 'taskset --all-tasks ' + '{} {}'
+    t1 = time.time()
+    for e in executables:
+        print(e)
+        P = subprocess.Popen( [ 'taskset', '--all-tasks', affinity_string, e ] )
+        P.wait()
+    t2 = time.time()
+    stats_pipe.send(t2-t1)
+
+
+class Workload:
+    def __init__(self, json_spec, base_path):
+        self.name = json_spec["title"]
+        self.path = base_path
+        self.microbenchmarks = json_spec["microbenchmarks"]
+        print(self.microbenchmarks)
+        self.parent_pipe = None
+        self.child_pipe = None
+        self.process = None
+        self.runtime = -1
+
+    # NOTE: Currently will run on just one thread anyways!
+    def run(self, affinity=0, threads=1):
+        try:
+            executables = [self.path + "/" + b + "/" + "runme_large.sh" for b in self.microbenchmarks]
+            # Open as a separate, persistent process:
+            affinity_string, num_threads = rand_affinity()
+            self.parent_pipe, self.child_pipe = Pipe()
+            self.process = Process(target=run_workload,
+                        args=(affinity_string, num_threads, executables, self.name, self.child_pipe) )
+            self.process.start()
+            self.runtime = -1
+            return True
+        except:
+            print("Error:", sys.exc_info()[0])
+            return False
+
+    # Join the current process (if exists) and return the runtime.
+    def join(self):
+        if self.process is None:
+            return False
+        else:
+            self.process.join()
+            self.runtime = self.parent_pipe.recv()
+            self.parent_pipe = None
+            self.child_pipe = None
+            self.process = None
+            return True
+
+    def get_last_runtime(self):
+        return self.runtime
+
+    def get_running_process(self):
+        return self.process
+
+    def is_running(self):
+        if self.process is None:
+            return False
+        else:
+            return self.process.is_alive()
+   
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return self.name
---
+++ random_workloads.py
@@ -0,0 +1,59 @@
+from multiprocessing import Process, Pipe
+import subprocess
+import random
+import time
+import os, sys
+import json 
+
+from Workload import *
+
+FNULL = open(os.devnull, 'w')
+
+bench_prob = 0.4
+period_max = 180
+bench_processes = []
+workloads = []
+path = os.getcwd()
+workloads_fname = "workload_collections.json"
+with open(workloads_fname, 'r') as wf:
+    json_data = json.load(wf)
+
+for workload_spec in json_data['workloads']:
+    if "microbenchmarks" in workload_spec:
+        workloads.append(Workload(workload_spec, path))
+        print("Added workload: {}".format(workloads[-1].name))
+
+print(workloads)
+
+random.seed()
+while True:
+    #for i in range(len(bench_processes)-1, -1, -1):
+    #if bench_processes[i].poll() is not None:
+    #    del bench_processes[i]
+    # Roll a die:
+    d = random.random()
+    if d <= bench_prob: # and len(bench_processes) == 0:
+        print("starting new workload run:")
+        # launch a new benchmark
+        bm_index = random.randint(0,len(workloads)-1)
+        # Launch the benchmark as a subprocess:
+        if workloads[bm_index].run():
+            bench_processes.append(workloads[bm_index].get_running_process())
+            print("Launched workload: {}".format(workloads[bm_index].name))
+            if not workloads[bm_index].join():
+                raise Exception("Subprocess failed to join!")
+            else:
+                runtime = workloads[bm_index].get_last_runtime()
+                name = workloads[bm_index].name
+                print("Runtime was {} seconds for {}.".format(runtime, name))
+        else:
+            print("Failed to launch {}".format(workloads[bm_index].name) )
+    else:
+        continue
+#   if len(bench_processes) > 0:
+        # Kill the process and go idle:
+        #bench_processes[0].kill()
+    wait_time = random.randint(0, period_max)
+    print("Waiting for {} minutes before spawning another process.".format(wait_time/60.0))
+    time.sleep(wait_time)
+
